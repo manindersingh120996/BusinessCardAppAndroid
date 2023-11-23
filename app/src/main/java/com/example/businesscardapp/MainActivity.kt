2ac31fa4ac9526a59a94df5035bbf95936ef13cf
manindersingh120996 <manindersingh120996@gmail.com>
2023-11-23 21:35:51 +0530

    Made Skeleton of Business card app

--- app/src/main/java/com/example/businesscardapp/MainActivity.kt
+++ app/src/main/java/com/example/businesscardapp/MainActivity.kt
@@ -3,14 +3,19 @@ package com.example.businesscardapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.businesscardapp.ui.theme.BusinessCardAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -40,18 +45,32 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 
 @Composable
 fun BusinessCard(modifier: Modifier = Modifier){
-    Column {
-
+    val img = painterResource(id = R.drawable.android_logo)
+    Column (horizontalAlignment = Alignment.CenterHorizontally){
+Image(
+painter = img,
+    contentDescription = "my image",
+    modifier = Modifier
+        .size(50.dp)
+)
+        Text(text = "Maninder Singh")
+Text(text="Senior Software Engineer")
     }
-    Column {
 
+
+    Column() {
+        Text(text = "+91-9**14****")
+        Text(text="https://medium.com/@manindersingh120996")
+
+        Text(text = "manindersingh120996@gmail.com")
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true,
+    showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     BusinessCardAppTheme {
-        Greeting("Android")
+        BusinessCard()
     }
 }
